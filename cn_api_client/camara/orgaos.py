b4dfcc0150d1f7649516341d9b5cb6dd770199ff
joao.carabetta@gmail.com <joao.carabetta@gmail.com>
2017-03-07 11:57:10 -0300

    Completed Camara API

--- cn_api_client/camara/orgaos.py
+++ cn_api_client/camara/orgaos.py
@@ -1,14 +1,6 @@
-"""
-ListarTiposOrgaos 	Retorna a lista dos tipos de órgãos que participam do processo legislativo na Câmara dos Deputados
-ObterAndamento 	Retorna o andamento de uma proposição pelos órgãos internos da Câmara a partir de uma data específica
-ObterEmendasSubstitutivoRedacaoFinal 	Retorna as emendas, substitutivos e redações finais de uma determinada proposição
-ObterIntegraComissoesRelator 	Retorna os dados de relatores e pareces, e o link para a íntegra de uma determinada proposição
-ObterMembrosOrgao 	Retorna os parlamentares membros de uma determinada comissão
-ObterOrgaos 	Retorna a lista de órgãos legislativos da Câmara dos Deputados (comissões, Mesa Diretora, conselhos, etc.)
-ObterPauta 	Retorna as pautas das reuniões de comissões e das sessões plenárias realizadas em um determinado período
-ObterRegimeTramitacaoDespacho 	Retorna os dados do último despacho da proposição"""
 from ..connection import Connection
 from ..utils import _make_url, _must_contain
+
 
 class OrgaosClient(Connection):
 
@@ -77,26 +69,161 @@ class OrgaosClient(Connection):
                                               base_url=base_url,
                                               params=params))
 
-    def obter_emendas_substitutivo_redacao_final(self):
+    def obter_emendas_substitutivo_redacao_final(self,
+                                                 tipo=None,
+                                                 ano=None,
+                                                 numero=None):
+        """
+        Retorna as emendas, substitutivos e redações finais de uma determinada proposição
 
-        print('emendas')
+        API ENDPOINT:
+        http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterEmendasSubstitutivoRedacaoFinal?tipo=PL&numero=3962&ano=2008
 
-    def obter_integra_comissoes_relator(self):
+        API DOC:
+        http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/orgaos/obteremendassubstitutivoredacaofinal
 
-        print('integra')
+        Args:
+            tipo:   String (Obrigatorio) :: Sigla do tipo de proposição
+            ano:    Int (Obrigatorio) 	 :: Numero da proposição
+            numero: Int (Obrigatorio) 	 :: Ano da proposição
+        """
 
-    def obter_membros_orgaos(self):
+        base_url = 'http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterEmendasSubstitutivoRedacaoFinal?'
+        params = dict([('tipo', tipo),
+                       ('numero', numero),
+                       ('ano', ano)])
 
-        print('membros')
+        _must_contain(this=params, keys=['ano', 'numero', 'tipo'])
 
+        return self.perform_request(_make_url(api_house='camara',
+                                              base_url=base_url,
+                                              params=params))
+
+    def obter_integra_comissoes_relator(self,
+                                        tipo=None,
+                                        ano=None,
+                                        numero=None):
+        """
+        Retorna os dados de relatores e pareces, e o link para a íntegra de uma determinada proposição
+
+        API ENDPOINT:
+        http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterIntegraComissoesRelator?tipo=PL&numero=3962&ano=2008
+
+        API DOC:
+        http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/orgaos/obterintegracomissoesrelator
+
+        Args:
+            tipo:   String (Obrigatorio) :: Sigla do tipo de proposição
+            ano:    Int (Obrigatorio) 	 :: Numero da proposição
+            numero: Int (Obrigatorio) 	 :: Ano da proposição
+        """
+
+        base_url = 'http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterIntegraComissoesRelator?'
+        params = dict([('tipo', tipo),
+                       ('numero', numero),
+                       ('ano', ano)])
+
+        _must_contain(this=params, keys=['ano', 'numero', 'tipo'])
+
+        return self.perform_request(_make_url(api_house='camara',
+                                              base_url=base_url,
+                                              params=params))
+
+
+
+    def obter_membros_orgaos(self, idOrgao=None):
+        """
+        Retorna os parlamentares membros de uma determinada comissão
+
+        API ENDPOINT:
+        http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterMembrosOrgao?IDOrgao=2004
+
+        API DOC:
+        http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/orgaos/obtermembrosorgao
+
+        Args:
+            idOrgao: Int (Obrigatorio)
+        """
+
+        base_url = 'http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterMembrosOrgao?'
+        params = dict([('idOrgao', idOrgao)])
+
+        _must_contain(this=params, keys=['idOrgao'])
+
+        return self.perform_request(_make_url(api_house='camara',
+                                              base_url=base_url,
+                                              params=params))
     def obter_orgaos(self):
+        """
+        Retorna a lista de órgãos legislativos da Câmara dos Deputados (comissões, Mesa Diretora, conselhos, etc.)
 
-        print('orgaos')
+        API ENDPOINT:
+        http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterOrgaos
 
-    def obter_pauta(self):
+        API DOC:
+        http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/orgaos/obterorgaos
 
-        print('pauta')
+        This method does not have input parameters
+        """
 
-    def obter_regime_tramitacao_despacho(self):
+        return self.perform_request('http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterOrgaos')
 
-        print('tramitacao')
+    def obter_pauta(self,
+                    idOrgao=None,
+                    dataIni=None,
+                    dataFim=None):
+        """
+        Retorna as pautas das reuniões de comissões e das sessões plenárias realizadas em um determinado período
+
+        API ENDPOINT:
+        http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterPauta?IDOrgao=2004&datIni=01/01/2012&datFim=30/04/2012
+
+        API DOC:
+        http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/orgaos/obterpauta
+
+        Args:
+            idOrgao: Int(Obrigatorio) 	ID do órgão (comissão) da Câmara dos Deputados
+            dataIni: String(Opcional) 	O métoto retorna a pauta das reuniões que foram realizadas em uma data maior ou igual a datIni
+            dataFim: String(Opcional) 	O métoto retorna a pauta das reuniões que foram realizadas em uma data menor ou igual a datFim
+        """
+
+        base_url = 'http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterPauta?'
+        params = dict([('idOrgao', idOrgao),
+                       ('dataIni', dataIni),
+                       ('dataFim', dataFim)])
+
+        _must_contain(this=params, keys=['idOrgao'])
+
+        return self.perform_request(_make_url(api_house='camara',
+                                              base_url=base_url,
+                                              params=params))
+
+    def obter_regime_tramitacao_despacho(self,
+                                         tipo=None,
+                                         numero=None,
+                                         ano=None):
+        """
+        Retorna os dados do último despacho da proposição
+
+        API ENDPOINT:
+        http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterRegimeTramitacaoDespacho?tipo=PL&numero=8035&ano=2010
+
+        API DOC:
+        http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/orgaos/obterregimetramitacaodespacho
+
+        Args:
+            tipo:   String (Obrigatorio) ::	 Sigla do tipo de proposição
+            numero: Int (Obrigatorio) 	 ::  Numero da proposição
+            ano:    Int (Obrigatorio)    ::	 Ano da proposição
+        """
+
+        base_url = 'http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterRegimeTramitacaoDespacho?'
+        params = dict([('tipo', tipo),
+                       ('numero', numero),
+                       ('ano', ano)])
+
+        _must_contain(this=params, keys=['tipo', 'ano', 'numero'])
+
+        return self.perform_request(_make_url(api_house='camara',
+                                              base_url=base_url,
+                                              params=params))
--- cn_api_client/camara/sessoesreunioes.py
+++ cn_api_client/camara/sessoesreunioes.py
@@ -1,29 +1,177 @@
-"""
-ListarDiscursosPlenario 	Retorna a lista dos deputados que proferiam discurso no Plenário da Cãmara dos Deputados em um determinado período.
-ListarPresencasDia 	Retorna a lista de presença de deputados em um determinado dia.
-ListarPresencasParlamentar 	Retorna as presenças de um deputado em um determinado período.
-ListarSituacoesReuniaoSessao 	Retorna a lista de situações para as reuniões de comissão e sessões plenárias da Câmara dos Deputados
-ObterInteiroTeorDiscursosPlenario 	Retorna o inteiro teor do discurso proferido no Plenário.
-"""
+from ..connection import Connection
+from ..utils import _make_url, _must_contain
 
-class SessoesClient:
+class SessoesClient(Connection):
 
-    def listar_discursos_plenario(self):
+    def listar_discursos_plenario(self,
+                                dataIni=None,
+                                data=None,
+                                dataFim=None,
+                                codigoSessao=None,
+                                parteNomeParlamentar=None,
+                                siglaPartido=None,
+                                siglaUF=None):
+        """
+        Retorna a lista dos deputados que proferiam discurso no Plenário da Câmara dos Deputados em um determinado período.
 
-        print('disc')
 
-    def listar_presencas_dia(self):
 
-        print('dia')
+        API ENDPOINT:
+        http://www.camara.gov.br/sitcamaraws/SessoesReunioes.asmx/ListarDiscursosPlenario?dataIni=23/11/2012&
+                                                                                          dataFim=23/11/2012&
+                                                                                          codigoSessao=&
+                                                                                          parteNomeParlamentar=&
+                                                                                          siglaPartido=&
+                                                                                          siglaUF=
 
-    def listar_presencas_parlamentar(self):
+        API DOC:
+        http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/sessoesreunioes-2/listardiscursosplenario
 
-        print('prese')
+        dataIni: 	            Date (Obrigatorio) ::   Data início do período desejado (formato: DD/MM/AAAA)
+        dataFim: 	            Date (Obrigatorio) :: 	Data de fim do período desejado (formato (DD/MM/AAAA)
+        codigoSessao: 	        String (Opcional)  ::	Código da sessão a ser pesquisada
+        parteNomeParlamentar: 	String (Opcional)  ::	Parte do nome do Deputado a ser pesquisada
+        siglaPartido: 	        String (Opcional)  ::	Sigla do Partido do Deputado
+        siglaUF: 	            String (Opcional)  ::	Sigla da UF do Deputado
+        """
+        base_url = 'http://www.camara.gov.br/sitcamaraws/SessoesReunioes.asmx/ListarDiscursosPlenario?'
+        params = dict([('dataIni', dataIni),
+                        ('dataFim', dataFim),
+                        ('codigoSessao', codigoSessao),
+                        ('parteNomeParlamentar', parteNomeParlamentar),
+                        ('siglaPartido', siglaPartido),
+                        ('siglaUF', siglaUF)])
+
+        _must_contain(params, ['dataIni', 'dataFim'])
+
+        return self.perform_request(_make_url(api_house='camara',
+                                              base_url=base_url,
+                                              params=params))
+
+    def listar_presencas_dia(self,
+                             data=None,
+                             numMatriculaParlamentar=None,
+                             siglaPartido=None,
+                             siglaUF=None):
+        """
+        Retorna a quantidade de sessões ocorridas no dia especificado e a presença dos parlamentares em cada sessão.
+
+        API ENDPOINT:
+        http://www.camara.gov.br/SitCamaraWS/sessoesreunioes.asmx/ListarPresencasDia?
+                                                                                    data=10/04/2012&
+                                                                                    numLegislatura=54&
+                                                                                    numMatriculaParlamentar=1&
+                                                                                    siglaPartido=&
+                                                                                    siglaUF=
+
+        API DOC:
+        http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/sessoesreunioes-2/listarpresencasdia
+
+        Args:
+            data: 	                  Date(Obrigatorio) :: Data da Sessão (formato: DD/MM/AAAA)
+            numMatriculaParlamentar:   Int(Opcional) 	:: Numero da matrícula do Parlamentar obtido pelo método ObterDeputados
+            siglaPartido: 	          String(Opcional) 	:: Sigla do Partido
+            siglaUF:	              String(Opcional) 	:: Sigla da UF a ser pesquisada
+        """
+        base_url = 'http://www.camara.gov.br/SitCamaraWS/sessoesreunioes.asmx/ListarPresencasDia?'
+        params = dict([('data', data),
+                        ('numMatriculaParlamentar', numMatriculaParlamentar),
+                        ('siglaPartido', siglaPartido),
+                        ('siglaUF', siglaUF),])
+
+
+
+        _must_contain(params, ['data'])
+
+        return self.perform_request(_make_url(api_house='camara',
+                                              base_url=base_url,
+                                              params=params))
+
+    def listar_presencas_parlamentar(self,
+                                     dataIni=None,
+                                     dataFim=None,
+                                     numMatriculaParlamentar=None,
+                                     ):
+        """
+        Retorna a quantidade de sessões ocorridas no Plenário em um período especificado e a presença dos parlamentares em cada sessão.
+
+        API ENDPOINT:
+        http://www.camara.gov.br/SitCamaraWS/sessoesreunioes.asmx/ListarPresencasParlamentar?
+                                                                                dataIni=20/11/2012&
+                                                                                dataFim=23/11/2012&
+                                                                                numMatriculaParlamentar=1
+
+        API DOC:
+        http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/sessoesreunioes-2/listarpresencasparlamentar
+
+        Args:
+            dataIni 	                Date(Obrigatorio) ::	Data inicial
+            dataFim 	                Date(Obrigatorio) ::	Data Final
+            numMatriculaParlamentar 	Int(Obrigatorio)  ::	Numero da matrícula do Parlamentar obtido pelo método ObterDeputados
+        """
+        base_url = 'http://www.camara.gov.br/SitCamaraWS/sessoesreunioes.asmx/ListarPresencasParlamentar?'
+        params = dict([('dataIni', dataIni),
+                        ('dataFim', dataFim),
+                        ('numMatriculaParlamentar', numMatriculaParlamentar)])
+
+
+        _must_contain(params, ['dataIni', 'dataFim', 'numMatriculaParlamentar'])
+
+        return self.perform_request(_make_url(api_house='camara',
+                                              base_url=base_url,
+                                              params=params))
 
     def listar_situacoes_reuniao_sessao(self):
+        """
+        Retorna a lista de situações para as reuniões de comissão e sessões plenárias da Câmara dos Deputados
 
-        print('sessao')
+        API ENDPOINT:
+        http://www.camara.gov.br/SitCamaraWS/SessoesReunioes.asmx/ListarSituacoesReuniaoSessao
 
-    def obter_inteiro_teor_discursos_plenario(self):
+        API DOC:
+        http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/sessoesreunioes-2/listarsituacoesreuniaosessao
 
-        print('teor')
+        This method does not have input parameters
+        """
+
+        return self.perform_request('http://www.camara.gov.br/SitCamaraWS/SessoesReunioes.asmx/ListarSituacoesReuniaoSessao')
+
+
+    def obter_inteiro_teor_discursos_plenario(self,
+                                            codSessao=None,
+                                            numOrador=None,
+                                            numQuarto=None,
+                                            numInsercao=None):
+        """
+        Retorna o inteiro teor de um discurso proferido no Plenário.
+
+        Modo de utilização: primeiro deve-se chamar o método ListarDiscursosPlenario para obtenção dos parâmetros
+        necessários para a identificação única do discurso desejado. São eles o código da sessão, o número do orador,
+        e mais 2 parâmetros numéricos, numQuarto e numInsercao.
+
+        API ENDPOINT:
+        http://www.camara.gov.br/SitCamaraWS/SessoesReunioes.asmx/obterInteiroTeorDiscursosPlenario?
+                                                                                            codSessao=022.3.54.O&
+                                                                                            numOrador=1&
+                                                                                            numQuarto=11&
+                                                                                            numInsercao=0
+        API DOC:
+        http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/sessoesreunioes-2/obterinteiroteordiscursosplenario
+
+        Args:
+            codSessao: 	    String (Obrigatorio) 	:: codigo que identifica uma sessão do Plenário
+            numOrador: 	    Inteiro (Obrigatorio) 	:: Identificador do orador na sessão
+            numQuarto: 	    Inteiro (Obrigatorio) 	:: Número da fração taquigráfica que identifica o início do discurso
+            numInsercao: 	Inteiro (Obrigatorio) 	:: Número da inserção taquigráfica que identifica o início do discurso
+        """
+        base_url = 'http://www.camara.gov.br/SitCamaraWS/SessoesReunioes.asmx/obterInteiroTeorDiscursosPlenario?'
+        params = dict([ ('codSessao', codSessao),
+                        ('numOrador', numOrador),
+                        ('numQuarto', numQuarto),
+                        ('numInsercao', numInsercao)])
+
+        _must_contain(params, ['codSessao','numOrador','numQuarto','numInsercao'])
+
+        return self.perform_request(_make_url(api_house='camara',
+                                              base_url=base_url,
+                                              params=params))
